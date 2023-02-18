77bee436e83309809e6575c28f9cb434cdcc12ef
SonjaWisnotavailable <sonjawharmby94@outlook.com>
2023-02-18 15:59:19 +0000

    Add functions and attributes to card functions

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,20 @@
-function onCardClicked (e){
-console.log('clicked',e.currentTarget);
+function onCardClicked(e) {
+
+const target= e.currentTarget; 
+console.log(target.className);
+
+target.className = target.className
+.replace('color-hidden')
+.trim();}
+
+if (!clickedCard) {
+
+    clickedCard =target;
 }
+
+else if (clickedCard) {
+
+    
+
+}
+
